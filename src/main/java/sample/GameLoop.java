dc0dc634e31a2337720164b3c093af38d23324ac
Mario Rubak <mario.rubak@stud.fh-campuswien.ac.at>
2019-11-19 00:44:36 +0100

    Resources moved and adaption of paths. Did some quick Intellij refactors e.G. setting some variables to private

--- src/main/java/sample/GameLoop.java
+++ src/main/java/sample/GameLoop.java
@@ -3,73 +3,71 @@ package sample;
 import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.media.Media;
 import javafx.util.Duration;
 
 import java.io.File;
 
 public class GameLoop extends Application {
 
-    static File splashFile = new File("src/media/splash.mp4");
-    static Media splashMedia = new Media(splashFile.toURI().toString());
-    static MediaPlayer splashPlayer = new MediaPlayer(splashMedia);
-    static MediaView splashView = new MediaView(splashPlayer);
-    static File ingamemusicFile = new File("src/media/sound/music/ingame2.mp3");
-    static Media ingamemusicMedia = new Media(ingamemusicFile.toURI().toString());
-    static MediaPlayer ingamemusicPlayer = new MediaPlayer(ingamemusicMedia);
-    static File gameovermusicFile = new File("src/media/sound/music/gameover1.mp3");
-    static Media gameovermusicMedia = new Media(gameovermusicFile.toURI().toString());
-    static MediaPlayer gameovermusicPlayer = new MediaPlayer(gameovermusicMedia);
-    static File eatsoundFile = new File("src/media/sound/eat2.mp3");
-    static Media eatsoundMedia = new Media(eatsoundFile.toURI().toString());
-    static MediaPlayer eatsoundPlayer = new MediaPlayer(eatsoundMedia);
-    static File deathsoundFile = new File("src/media/sound/death1.mp3");
-    static Media deathsoundMedia = new Media(deathsoundFile.toURI().toString());
-    static MediaPlayer deathsoundPlayer = new MediaPlayer(deathsoundMedia);
-    Group root = new Group();
-    Pane backgroundPane = new Pane(); //TODO NEU für Background
-    Group splashscreen = new Group();
+    private static File splashFile = new File("src/main/resources/media/splash.mp4");
+    private static Media splashMedia = new Media(splashFile.toURI().toString());
+    private static MediaPlayer splashPlayer = new MediaPlayer(splashMedia);
+    private static MediaView splashView = new MediaView(splashPlayer);
+    private static File ingamemusicFile = new File("src/main/resources/media/sound/music/ingame2.mp3");
+    private static Media ingamemusicMedia = new Media(ingamemusicFile.toURI().toString());
+    private static MediaPlayer ingamemusicPlayer = new MediaPlayer(ingamemusicMedia);
+    private static File gameovermusicFile = new File("src/main/resources/media/sound/music/gameover1.mp3");
+    private static Media gameovermusicMedia = new Media(gameovermusicFile.toURI().toString());
+    private static MediaPlayer gameovermusicPlayer = new MediaPlayer(gameovermusicMedia);
+    private static File eatsoundFile = new File("src/main/resources/media/sound/eat2.mp3");
+    private static Media eatsoundMedia = new Media(eatsoundFile.toURI().toString());
+    private static MediaPlayer eatsoundPlayer = new MediaPlayer(eatsoundMedia);
+    private static File deathsoundFile = new File("src/main/resources/media/sound/death1.mp3");
+    private static Media deathsoundMedia = new Media(deathsoundFile.toURI().toString());
+    private static MediaPlayer deathsoundPlayer = new MediaPlayer(deathsoundMedia);
+    private Group root = new Group();
+    private Pane backgroundPane = new Pane(); //TODO NEU für Background
+    private Group splashscreen = new Group();
     //TODO NEU - Background stuff
-    Image imgSource;
-    BackgroundImage backgroundImage;
-    Background backgroundView;
+    private Image imgSource;
+    private BackgroundImage backgroundImage;
+    private Background backgroundView;
     private long lastUpdate = 0; //für Geschwindigkeitssteuerung
 
-    public static void restartIngamemusic() { //Startet Ingame Musik von vorne
+    static void restartIngamemusic() { //Startet Ingame Musik von vorne
         ingamemusicPlayer.seek(Duration.ZERO);
         ingamemusicPlayer.play();
     }
 
-    public static void stopIngamemusic() {
+    static void stopIngamemusic() {
         ingamemusicPlayer.stop();
     }
 
-    public static void restartGameovermusic() {
+    static void restartGameovermusic() {
         gameovermusicPlayer.seek(Duration.ZERO);
         gameovermusicPlayer.play();
     }
 
-    public static void stopGameovermusic() {
+    static void stopGameovermusic() {
         gameovermusicPlayer.stop();
     }
 
-    public static void playEatsound() {
+    static void playEatsound() {
         eatsoundPlayer.seek(Duration.ZERO);
         eatsoundPlayer.play();
     }
 
-    public static void playDeathsound() {
+    static void playDeathsound() {
         deathsoundPlayer.seek(Duration.ZERO);
         deathsoundPlayer.play();
     }
@@ -82,8 +80,8 @@ public class GameLoop extends Application {
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        AnimationTimer timer;
+    public void start(final Stage primaryStage) {
+        final AnimationTimer timer;
 
         primaryStage.setWidth(1500);
         primaryStage.setHeight(700);
@@ -93,28 +91,28 @@ public class GameLoop extends Application {
 
         //TODO NEU - Background stuff
         imgSource = new Image("media/grassTile.png");
-        backgroundImage = new BackgroundImage(imgSource, BackgroundRepeat.REPEAT.REPEAT, BackgroundRepeat.REPEAT.REPEAT,
+        backgroundImage = new BackgroundImage(imgSource, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
                 BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         backgroundView = new Background(backgroundImage);
         backgroundPane.setBackground(backgroundView);
         //TODO END Background
 
 
-        int offset = 21; //TODO Variable Namen anpassen
-        Gameboard gameboard = new Gameboard(); // TODO NEW
-        Control control = new Control();
-        Snake snake = new Snake(root, primaryStage); //erstellt neues Snake Listen Objekt und getChilded es
-        GameObject food = new GameObject();
-        Score score = new Score(root);
+        final int offset = 21; //TODO Variable Namen anpassen
+        final Gameboard gameboard = new Gameboard(); // TODO NEW
+        final Control control = new Control();
+        final Snake snake = new Snake(root, primaryStage); //erstellt neues Snake Listen Objekt und getChilded es
+        final GameObject food = new GameObject();
+        final Score score = new Score(root);
         food.setFood(root, primaryStage);//setzt ein neues Food random ab
-        Scene scene = new Scene(backgroundPane, primaryStage.getWidth(), primaryStage.getHeight(), Color.DARKGREEN);
+        final Scene scene = new Scene(backgroundPane, primaryStage.getWidth(), primaryStage.getHeight(), Color.DARKGREEN);
         backgroundPane.getChildren().add(root); //TODO NEU Background - root (Group) zu backgroundPane als Child added
 
         Rectangle blackrect = new Rectangle();  //Schwarzer Block der für eine Szenentransition missbraucht wird
         blackrect.setFill(Color.BLACK);
         blackrect.setHeight(primaryStage.getHeight());
         blackrect.setWidth(primaryStage.getWidth());
-        FadeTransition fadeblacktotransparent = new FadeTransition(Duration.millis(700), blackrect);
+        final FadeTransition fadeblacktotransparent = new FadeTransition(Duration.millis(700), blackrect);
         fadeblacktotransparent.setFromValue(1.0);
         fadeblacktotransparent.setToValue(0.0);
         root.getChildren().add(blackrect);
@@ -139,12 +137,10 @@ public class GameLoop extends Application {
             }
         });
         */
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {//Keyeventhandler fragt ab obs ein Keyevent gibt
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                control.keyHandler(keyEvent, snake, root, food, score, primaryStage);//control nimmt Keyevent und schaut speziell nach WASD
+        //Keyeventhandler fragt ab obs ein Keyevent gibt
+        scene.setOnKeyPressed(keyEvent -> {
+            control.keyHandler(keyEvent, snake, root, food, score, primaryStage);//control nimmt Keyevent und schaut speziell nach WASD
 
-            }
         });
 
 
@@ -169,14 +165,11 @@ public class GameLoop extends Application {
                 }
             }
         };
-        splashPlayer.setOnEndOfMedia(new Runnable() {
-            @Override
-            public void run() {
-                primaryStage.setScene(scene);
-                fadeblacktotransparent.play();
-                timer.start(); //Animationtimer startet nun erst nach dem Fade out des Hundevideos
-                restartIngamemusic();
-            }
+        splashPlayer.setOnEndOfMedia(() -> {
+            primaryStage.setScene(scene);
+            fadeblacktotransparent.play();
+            timer.start(); //Animationtimer startet nun erst nach dem Fade out des Hundevideos
+            restartIngamemusic();
         });
 
     }
